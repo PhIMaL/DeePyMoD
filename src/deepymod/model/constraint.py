92d4ae9bcb89d803c817ab2d3c94383e060b5e7f
Gert-Jan <gertjanboth@gmail.com>
2020-12-11 10:42:57 +0100

    Added ridge constraint.

--- src/deepymod/model/constraint.py
+++ src/deepymod/model/constraint.py
@@ -57,3 +57,37 @@ class GradParams(Constraint):
             (TensorList): Calculated coefficients of size (n_features, n_outputs).
         """
         return self.coeffs
+
+
+class Ridge(Constraint):
+    """Implements the constraint as a least squares problem solved by QR decomposition. """
+
+    def __init__(self, l=1e-3) -> None:
+        super().__init__()
+        self.l = torch.tensor(l)
+
+    def fit(self, sparse_thetas: TensorList, time_derivs: TensorList) -> TensorList:
+        """Calculates the coefficients of the constraint using the QR decomposition for every pair
+        of sparse feature matrix and time derivative.
+
+        Args:
+            sparse_thetas (TensorList): List containing the sparse feature tensors.
+            time_derivs (TensorList): List containing the time derivatives.
+
+        Returns:
+            (TensorList): Calculated coefficients.
+        """
+        coeff_vectors = []
+        for theta, dt in zip(sparse_thetas, time_derivs):
+            # We use the augmented data method
+            norm = torch.norm(theta, dim=0)
+            l_normed = torch.diag(
+                torch.sqrt(self.l) * norm
+            )  # we norm l rather than theta cause we want unnormed coeffs out
+            X = torch.cat((theta, l_normed), dim=0)
+            y = torch.cat((dt, torch.zeros((theta.shape[1], 1))), dim=0)
+            # Now solve like normal OLS prob lem
+            Q, R = torch.qr(X)  # solution of lst. sq. by QR decomp.
+            coeff_vectors.append(torch.inverse(R) @ Q.T @ y)
+
+        return coeff_vectors
