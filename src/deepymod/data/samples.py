98c844498c6dfd28a8e8cffb341d4924732e7da5
Gijs vermariën <gijs.vermarien@gmail.com>
2021-04-04 12:10:05 +0200

    Updated subsamplers

--- src/deepymod/data/samples.py
+++ src/deepymod/data/samples.py
@@ -13,40 +13,33 @@ class Subsampler(ABC, metaclass=ABCMeta):
 
 class Subsample_grid(Subsampler):
     @staticmethod
-    def sample(grid, grid_data, number_of_samples):
+    def sample(coords, data, number_of_samples):
         print(number_of_samples)
         """Subsample on the second axis for data in the format [u, x, t]"""
         # getting indices of samples
         x_idx = torch.linspace(
-            0, grid.shape[1] - 1, number_of_samples, dtype=torch.long
+            0, coords.shape[1] - 1, number_of_samples, dtype=torch.long
         )  # getting x locations
         # getting sample locations from indices
-        subsampled_coords = torch.tensor(grid[:, :, x_idx].reshape(-1, 2))
-        subsampled_data = torch.tensor(grid_data[:, :, x_idx].reshape(-1, 1))
+        subsampled_coords = coords[:, :, x_idx]
+        subsampled_data = data[:, :, x_idx]
         return subsampled_coords, subsampled_data
 
 
 class Subsample_shifted_grid(Subsampler):
     @staticmethod
-    def sub_sample_shifted_grid(grid, grid_data, number_of_samples):
-        # getting indices of samples
-        x_idx = torch.linspace(
-            0, grid.shape[1] - 1, number_of_samples, dtype=torch.long
-        )  # getting x locations
-        # getting sample locations from indices
-        subsampled_coords = torch.tensor(grid[:, :, x_idx].reshape(-1, 2))
-        subsampled_data = torch.tensor(grid_data[:, :, x_idx].reshape(-1, 1))
-        return subsampled_coords, subsampled_data
+    def sub_sample_shifted_grid(coords, data, number_of_samples):
+        return NotImplementedError
 
 
 class Subsample_random(Subsampler):
     @staticmethod
-    def sub_sample_random(grid, grid_data, number_of_samples):
+    def sub_sample_random(coords, data, number_of_samples):
         # getting indices of samples
-        x_idx = torch.linspace(
-            0, grid.shape[1] - 1, number_of_samples, dtype=torch.long
-        )  # getting x locations
+        x_idx = torch.randperm(coords.shape[1])[
+            :number_of_samples
+        ]  # getting x locations
         # getting sample locations from indices
-        subsampled_coords = torch.tensor(grid[:, :, x_idx].reshape(-1, 2))
-        subsampled_data = torch.tensor(grid_data[:, :, x_idx].reshape(-1, 1))
+        subsampled_coords = coords[:, :, x_idx]
+        subsampled_data = data[:, :, x_idx]
         return subsampled_coords, subsampled_data
